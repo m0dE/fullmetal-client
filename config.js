ad8cc3f7be658e514b746cbe77cccd1a41ef5625
shaileshminsnapsys <75966330+shaileshminsnapsys@users.noreply.github.com>
2023-10-10 15:13:53 +0530

    Updating the api url

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const Rollbar = require('rollbar');
 
 exports.APIURL = 'https://api.fullmetal.ai/';
-// exports.APIURL = 'http://localhost:5000/';
 
 exports.rollbar = new Rollbar({
   accessToken: '1102c4abd7d04345a6a58be5a06cdeb8',
